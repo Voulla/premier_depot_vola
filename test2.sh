d2aca8f8becf18b2c478e6dbcfeb3e2108b97f25
Ubuntu <ubuntu@ip-172-31-19-165.eu-west-1.compute.internal>
2026-02-04 16:15:11 +0000

    fichier2.txt

--- test2.sh
+++
@@ -1,8 +0,0 @@
-prenom="Diane"
-nom="Datascientest"
-if [ "$prenom" = "Diane" ] && [ "$nom" = "Datascientest" ]
-then
-    echo "Bonjour Diane de Datascientest !"
-else
-    echo "Bonjour $prenom $nom"
-fi
